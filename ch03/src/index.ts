3a42c8af050429683b9b2ce186f24f2cabe88aeb
leeyoonseo <okayoon.lee@gmail.com>
2021-12-20 22:39:30 +0900

    interface 불일치 조건

--- ch03/src/index.ts
+++ ch03/src/index.ts
@@ -1,5 +1,10 @@
-let count = 10
-let message = 'Your count'
-let result = `${message} is ${count}`
+interface IPerson { 
+  name: string,
+  age: number
+}
 
-console.log(result)
+let good: IPerson = { name: 'Jack', age: 32 } // 일치
+let bad1: IPerson = { name: 'Jack' } // age가 없으므로 불일치
+let bad2: IPerson = { age: 32 } // name이 없으므로 불일치
+let bad3: IPerson = {} // 값이 없으므로 불일치
+let bad4: IPerson = { name: 'Jack', age: 32, etc: true } // etc가 있으므로 불일치
