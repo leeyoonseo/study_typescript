df845790229c1d4e94995015ce6937b4d591c433
leeyoonseo <okayoon.lee@gmail.com>
2021-12-20 22:40:56 +0900

    선택 속성(optional property)

--- ch03/src/index.ts
+++ ch03/src/index.ts
@@ -1,10 +1,12 @@
 interface IPerson { 
-  name: string,
-  age: number
+  name: string, // 필수
+  age: number, // 필수
+  etc?: boolean // 선택
 }
 
-let good: IPerson = { name: 'Jack', age: 32 } // 일치
 let bad1: IPerson = { name: 'Jack' } // age가 없으므로 불일치
 let bad2: IPerson = { age: 32 } // name이 없으므로 불일치
 let bad3: IPerson = {} // 값이 없으므로 불일치
-let bad4: IPerson = { name: 'Jack', age: 32, etc: true } // etc가 있으므로 불일치
+
+let good1: IPerson = { name: 'Jack', age: 32 } // 일치
+let good2: IPerson = { name: 'Jack', age: 32, etc: true } // 선택 속성으로 인해 일치
