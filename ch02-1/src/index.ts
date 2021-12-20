a8605735970fa62f13eedec9cdc866aaab44f23f
leeyoonseo <okayoon.lee@gmail.com>
2021-12-20 21:59:07 +0900

    typescript 모듈 분리 학습

--- ch02-1/src/index.ts
+++ ch02-1/src/index.ts
@@ -1,2 +1,10 @@
-import { testMakePerson } from './utils/makePerson'
-testMakePerson();
+import IPerson from './person/Iperson'
+import Person, { makePerson } from "./person/Person"
+
+const testMakePerson = (): void => {
+  let jane: IPerson = makePerson('Jane')
+  let jack: IPerson = new Person('Jack')
+  console.log(jane, jack)
+}
+
+testMakePerson()
