879a98fca8fc91bfa70a5041f2eda117acaaab98
leeyoonseo <okayoon.lee@gmail.com>
2021-12-20 22:43:47 +0900

    익명 인터페이스 (anonymous interface)

--- ch03/src/index.ts
+++ ch03/src/index.ts
@@ -1,12 +1,16 @@
-interface IPerson { 
-  name: string, // 필수
-  age: number, // 필수
-  etc?: boolean // 선택
+let ai: {
+  name: string,
+  age: number,
+  etc?: boolean
+} = { name: 'Jack', age: 32 }
+
+// 주 예시
+function printMe(me: { name: string, age: number, etc?: boolean }) {
+  console.log(
+    me.etc ? 
+      `${me.name} ${me.age} ${me.etc}` :
+      `${me.name} ${me.age}`
+  )
 }
 
-let bad1: IPerson = { name: 'Jack' } // age가 없으므로 불일치
-let bad2: IPerson = { age: 32 } // name이 없으므로 불일치
-let bad3: IPerson = {} // 값이 없으므로 불일치
-
-let good1: IPerson = { name: 'Jack', age: 32 } // 일치
-let good2: IPerson = { name: 'Jack', age: 32, etc: true } // 선택 속성으로 인해 일치
+printMe(ai) // Jack 32
