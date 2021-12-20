78056ff42109678b94bd2b550ec23d56c1f22feb
leeyoonseo <okayoon.lee@gmail.com>
2021-12-20 22:32:42 +0900

    타입 추론 (type inference)

--- ch03/src/index.ts
+++ ch03/src/index.ts
@@ -1,9 +1,9 @@
-let n: number = 1
-let b: boolean = true; // or false
-let s: string = 'hello'
-let o: object = {}
+let n = 1 // number
+let b = true; // boolean
+let s = 'hello' // string
+let o = {} // object
 
-// 타입 불일치 오류
+// 타입 추론에 의해 여전히 타입 불일치 오류
 n = 'a'
 b = 1
 s = false
