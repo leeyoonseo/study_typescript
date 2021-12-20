ad93184737c3f5d19b5a299443a9cf572bfa4038
leeyoonseo <okayoon.lee@gmail.com>
2021-12-20 22:08:05 +0900

    모듈이해하기 - 외부 모듈 사용 테스트

--- ch02-1/src/index.ts
+++ ch02-1/src/index.ts
@@ -1,10 +1,8 @@
+import Chance from 'chance'
+import * as R from 'ramda'
 import IPerson from './person/Iperson'
 import Person, { makePerson } from "./person/Person"
 
-const testMakePerson = (): void => {
-  let jane: IPerson = makePerson('Jane')
-  let jack: IPerson = new Person('Jack')
-  console.log(jane, jack)
-}
-
-testMakePerson()
+const chance = new Chance()
+let persons: IPerson[] = R.range(0, 2).map((n: number) => new Person(chance.name(), chance.age()))
+console.log(persons)
