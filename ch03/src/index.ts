425df5cb6efc6babdf0caee0c948d376266d9604
leeyoonseo <okayoon.lee@gmail.com>
2021-12-20 22:33:30 +0900

    any

--- ch03/src/index.ts
+++ ch03/src/index.ts
@@ -1,12 +1,6 @@
-let n = 1 // number
-let b = true; // boolean
-let s = 'hello' // string
-let o = {} // object
+let a: any = 0;
 
-// 타입 추론에 의해 여전히 타입 불일치 오류
-n = 'a'
-b = 1
-s = false
-
-// 타입 일치
-o = { name: 'Jack', age: 32 }
+// any로 인해 어떤 타입이든 가능
+a = 'hello'
+a = false
+a = {}
