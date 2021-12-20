770035709e7b18e59a6b4203e68150aadd04caca
leeyoonseo <okayoon.lee@gmail.com>
2021-12-20 22:30:17 +0900

    타입 주석(type annotaion)

---
+++ ch03/src/index.ts
@@ -0,0 +1,12 @@
+let n: number = 1
+let b: boolean = true; // or false
+let s: string = 'hello'
+let o: object = {}
+
+// 타입 불일치 오류
+n = 'a'
+b = 1
+s = false
+
+// 타입 일치
+o = { name: 'Jack', age: 32 }
