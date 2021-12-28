5a6d175a6925d1986e5d176e86afd3b65a86cd35
leeyoonseo <okayoon.lee@gmail.com>
2021-12-28 21:18:38 +0900

    객체 반환시 주의

--- ch04/src/index.ts
+++ ch04/src/index.ts
@@ -1,10 +1,9 @@
 type Person = { name: string, age: number }
 
-// * 매개변수 기본 값
-const makePerson = (name: string, age: number = 10): Person => {
-  const person = { name, age }
-  return person
-}
+// * 객체 반환 시 주의
+// - 객체를 소괄호로 감싸주어야 컴파일러가 옳바르게 인식
+// const makePerson = (name: string, age: number = 10): Person => { name, age } // X
+const makePerson = (name: string, age: number = 10): Person => ({ name, age }) // O
 
 console.log(makePerson('Jack')) // { name: 'Jack', age: 10 }
 console.log(makePerson('Jack', 33)) // { name: 'Jack', age: 33 }
