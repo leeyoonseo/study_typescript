e7709ec8960a4663ba5f5aa222e90a01bbf045ba
leeyoonseo <okayoon.lee@gmail.com>
2021-12-28 20:11:25 +0900

    추상클래스
    - 이해가 잘..

--- ch03/src/index.ts
+++ ch03/src/index.ts
@@ -1,25 +1,10 @@
-class Person3 {
-  // 멤버 변수 선언?
-  name: string
-  age?: Number
+abstract class Person {
+  abstract name: string
+  constructor(public age?: number) {}
+}
 
-  constructor(name: string, age?: number) {
-    this.name = name
-    this.age = age
+class Jack extends Person {
+  constructor(public name: string, age?: number) {
+    super(age)
   }
 }
-
-let jack3: Person3 = new Person3('Jack', 32);
-console.log(jack3)
-
-interface IPerson4 {
-  name: string
-  age?: number
-}
-
-class Person4 implements IPerson4 {
-  // 멤버 속성으로 포함?
-  constructor(public name: string, public age?: number) {}
-}
-let jack4: IPerson4 = new Person4('Jack', 32)
-console.log(jack4)
