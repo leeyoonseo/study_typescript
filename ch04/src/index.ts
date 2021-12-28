a6f1a90e511bc4d95e7f881a6468c62477362338
leeyoonseo <okayoon.lee@gmail.com>
2021-12-28 20:41:37 +0900

    type 키워드로 타입 별칭 만들기

--- ch04/src/index.ts
+++ ch04/src/index.ts
@@ -1,9 +1,15 @@
-// * 함수의 타입 = 함수 시그니처(function signature)
-// * 형태
-// (매개변수1 타입, 매개변수2 타입[, ...]) => 반환값 타입
+// * type 키워드로 타입 별칭 만들기
+type stringNumberFunc = (string, number) => void
 
-let printMe: (string, number) => void = function (name: string, age: number): void {
+let f: stringNumberFunc = function (a: string, b: number): void {
   // ....
 }
 
-// * 매개변수와 반환값이 없을 경우 () => void
+let g: stringNumberFunc = function (c: string, d: number): void {
+  // ....
+}
+
+// * 오류나는 상황 확인
+let h: stringNumberFunc = function () {
+  // ...
+}
