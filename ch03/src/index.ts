e1433bd68554cf2442ac4ed81fe971e3b73263d2
leeyoonseo <okayoon.lee@gmail.com>
2021-12-28 20:20:49 +0900

    전개 연산자

--- ch03/src/index.ts
+++ ch03/src/index.ts
@@ -1,10 +1,5 @@
-let address: any = {
-  country: 'Korea',
-  city: 'Seoul',
-  address1: 'Gangnam-gu',
-  address2: 'Sinsa-dong 123-456',
-  address3: '789 street, 2 Floor ABC building'
-}
-
-const { country, city, ...detail } = address
-console.log(detail)
+let part1 = { name: 'Jack' }
+let part2 = { age: 22 }
+let part3 = { city: 'Seoul', country: 'Kr' }
+let merged = { ...part1, ...part2, ...part3 }
+  
