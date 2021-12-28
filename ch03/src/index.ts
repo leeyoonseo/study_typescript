e6966f2acfc20f5d4f24d7ff88e064d2d29f2817
leeyoonseo <okayoon.lee@gmail.com>
2021-12-28 20:26:13 +0900

    타입 변환 구문

--- ch03/src/index.ts
+++ ch03/src/index.ts
@@ -1,5 +1,8 @@
-let part1 = { name: 'Jack' }
-let part2 = { age: 22 }
-let part3 = { city: 'Seoul', country: 'Kr' }
-let merged = { ...part1, ...part2, ...part3 }
-  
+// * person의 변수 타입은 object
+// * object 타입은 name 속성을 가지지 않으므로 오류
+// let person: object = { name: 'Jack' }
+// console.log(person.name)
+
+// * 타입 변환 구문을 통해 일시적으로 속성 값 주입
+let person: object = { name: 'Jack' }
+console.log((<{name: string}>person).name)
