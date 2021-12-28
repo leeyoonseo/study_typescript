69b26791fa0e39670073dad0e930778ba1f0b1aa
leeyoonseo <okayoon.lee@gmail.com>
2021-12-28 21:20:52 +0900

    매개변수 비구조화 할당

--- ch04/src/index.ts
+++ ch04/src/index.ts
@@ -1,9 +1,8 @@
 type Person = { name: string, age: number }
 
-// * 객체 반환 시 주의
-// - 객체를 소괄호로 감싸주어야 컴파일러가 옳바르게 인식
-// const makePerson = (name: string, age: number = 10): Person => { name, age } // X
-const makePerson = (name: string, age: number = 10): Person => ({ name, age }) // O
+// 매개변수 비구조화 할당
+const printPerson = ({name, age}: Person): void => {
+  console.log(`name: ${name}, age: ${age}`)
+}
 
-console.log(makePerson('Jack')) // { name: 'Jack', age: 10 }
-console.log(makePerson('Jack', 33)) // { name: 'Jack', age: 33 }
+printPerson({ name: 'Jack', age: 33 })
