cd3e4888331e1bf37975641239a336218ed9fdb9
leeyoonseo <okayoon.lee@gmail.com>
2021-12-28 20:14:00 +0900

    구조화

--- ch03/src/index.ts
+++ ch03/src/index.ts
@@ -1,10 +1,10 @@
-abstract class Person {
-  abstract name: string
-  constructor(public age?: number) {}
-}
+import { IPerson, ICompany } from './interface'
 
-class Jack extends Person {
-  constructor(public name: string, age?: number) {
-    super(age)
-  }
-}
+let jack: IPerson = { name: 'Jack', age: 32 }
+let jane: IPerson = { name: 'jane', age: 22 }
+
+let apple: ICompany = { name: 'apple', age: 43 }
+let ms: ICompany = { name: 'ms', age: 44 }
+
+console.log(jack)
+console.log(ms)
---
+++ ch03/src/interface.ts
@@ -0,0 +1,10 @@
+// 구조화
+export interface IPerson {
+  name: string
+  age: number
+}
+
+export interface ICompany {
+  name: string
+  age: number
+}
