bcd7bfbcc32a6ec382e5a79cf2a63626a8cbe2e0
leeyoonseo <okayoon.lee@gmail.com>
2021-12-28 20:28:46 +0900

    타입단언

--- ch03/src/index.ts
+++ ch03/src/index.ts
@@ -1,8 +1,11 @@
-// * person의 변수 타입은 object
-// * object 타입은 name 속성을 가지지 않으므로 오류
-// let person: object = { name: 'Jack' }
-// console.log(person.name)
+// 타입변환 = 타입단언
+// (<타입>객체)
+// (객체 as 타입)
 
-// * 타입 변환 구문을 통해 일시적으로 속성 값 주입
-let person: object = { name: 'Jack' }
-console.log((<{name: string}>person).name)
+import { INameable } from './interface'
+
+let obj: object = { name: 'Jack' }
+
+let name1 = (<INameable>obj).name
+let name2 = (obj as INameable).name
+console.log(name1, name2)
--- ch03/src/interface.ts
+++ ch03/src/interface.ts
@@ -1,10 +1,3 @@
-// 구조화
-export interface IPerson {
+export interface INameable {
   name: string
-  age: number
-}
-
-export interface ICompany {
-  name: string
-  age: number
 }
