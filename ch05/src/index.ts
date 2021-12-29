cc44c0e5faaf8634901796748c58b2ee1a51f303
leeyoonseo <okayoon.lee@gmail.com>
2021-12-29 20:52:26 +0900

    명령형, 선언형 차이 (fold 구현)

--- ch05/src/index.ts
+++ ch05/src/index.ts
@@ -4,5 +4,19 @@ const range = (from: number, to: number): number[] => {
     : []
 }
 
-let numbers: number[] = range(1, 9 + 1)
-console.log(numbers)
+const fold = <T>(array: T[], callback: (result: T, val: T) => T, initValue: T) => {
+  let result: T = initValue;
+  for(let i = 0; i < array.length; i++) {
+    const value = array[i]
+    result = callback(result, value)
+  }
+
+  return result
+}
+
+// 입력 데이터 생성
+let numbers: number[] = range(1, 100 + 1)
+
+// 입력 데이터 가공
+let result = fold(numbers, (result, value) => result + value, 0)
+console.log(result)
