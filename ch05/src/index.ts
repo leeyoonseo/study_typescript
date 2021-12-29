081e1bc6da421f2aa75415ce93c5f8471c4a4733
leeyoonseo <okayoon.lee@gmail.com>
2021-12-29 19:58:18 +0900

    배열 타입

---
+++ ch05/src/index.ts
@@ -0,0 +1,8 @@
+let numArr: number[] = [1,2,3]
+let strArr: string[] = ['a','b','c']
+
+type IPerson = { name: string, age?: number }
+let personArr: IPerson[] = [
+  { name: 'Jack', age: 33 },
+  { name: 'Jane', age: 23 }
+]
