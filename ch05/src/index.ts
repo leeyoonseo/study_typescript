61ffb03ed6eec4fe7742b447ce74039576c08422
leeyoonseo <okayoon.lee@gmail.com>
2021-12-29 20:02:57 +0900

    문자열과 배열간 변환

--- ch05/src/index.ts
+++ ch05/src/index.ts
@@ -6,3 +6,13 @@ let personArr: IPerson[] = [
   { name: 'Jack', age: 33 },
   { name: 'Jane', age: 23 }
 ]
+
+// 문자열과 배열 간 변환
+// split(구분자: string): string[]
+const split = (str: string, delim: string = ''): string[] => str.split(delim)
+console.log(split('hello'))
+console.log(split('h_e_l_l_o', '_'))
+
+const join = (strArr: string[], delim: string = ''): string => strArr.join(delim)
+console.log(['h', 'e', 'l', 'l', 'o'])
+console.log(['h', 'e', 'l', 'l', 'o'], '_')
