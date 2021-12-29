5bcdaa4677048b44ed2f1e65f3e5b26740f848a0
leeyoonseo <okayoon.lee@gmail.com>
2021-12-29 20:21:36 +0900

    range함수 구현

--- ch05/src/index.ts
+++ ch05/src/index.ts
@@ -1,9 +1,8 @@
-const identity = <T>(n: T): T => n
-console.log(
-  identity<boolean>(true),
-  identity(true),
-)
+const range = (from: number, to: number): number[] => {
+  return from < to   
+    ? [from, ...range(from + 1, to)]
+    : []
+}
 
-// 제네릭 형태로 구현된 함수는 원칙적으로 다음과 같은 형태로 명시
-// 함수이름<타입변수>(매개변수)
-// <타입변수>가 생략된 경우 타입 추론을 통해 생략된 타입을 찾아낸다
+let numbers: number[] = range(1, 9 + 1)
+console.log(numbers)
