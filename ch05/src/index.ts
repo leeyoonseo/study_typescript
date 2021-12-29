7d47ceaef75ae887318d0782db7d9e8841fdfa8b
leeyoonseo <okayoon.lee@gmail.com>
2021-12-29 21:02:05 +0900

    함수형 filter

--- ch05/src/index.ts
+++ ch05/src/index.ts
@@ -1,5 +1,36 @@
-let oddSum = 0
-for (let i = 1; i <= 100; i += 2)
-  oddSum += i
+const range = (from: number, to: number): number[] => {
+  return from < to   
+    ? [from, ...range(from + 1, to)]
+    : []
+}
 
-console.log(oddSum)
+const fold = <T>(array: T[], callback: (result: T, val: T) => T, initValue: T) => {
+  let result: T = initValue;
+  for(let i = 0; i < array.length; i++) {
+    const value = array[i]
+    result = callback(result, value)
+  }
+
+  return result
+}
+
+const filter = <T>(array: T[], callback: (value: T, index?: number) => boolean): T[] => {
+  let result: T[] = []
+  for (let index: number = 0; index < array.length; ++index) {
+    const value = array[index]
+    if (callback(value, index)) {
+      result = [...result, value]
+    }
+
+    return result
+  }
+}
+
+let numbers: number[] = range(1, 100 + 1)
+const isOdd = (n: number): boolean => n % 2 != 0
+let result = fold(
+  filter(numbers, isOdd),
+  (result, value) => result + value, 0
+)
+
+console.log(result)
