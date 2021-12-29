dfdd50aefd759d30be6dcb022307edde3b9f8402
leeyoonseo <okayoon.lee@gmail.com>
2021-12-29 20:54:04 +0900

    명령형 방식 홀수추리기

--- ch05/src/index.ts
+++ ch05/src/index.ts
@@ -1,22 +1,5 @@
-const range = (from: number, to: number): number[] => {
-  return from < to   
-    ? [from, ...range(from + 1, to)]
-    : []
-}
+let oddSum = 0
+for (let i = 1; i <= 100; i += 2)
+  oddSum += i
 
-const fold = <T>(array: T[], callback: (result: T, val: T) => T, initValue: T) => {
-  let result: T = initValue;
-  for(let i = 0; i < array.length; i++) {
-    const value = array[i]
-    result = callback(result, value)
-  }
-
-  return result
-}
-
-// 입력 데이터 생성
-let numbers: number[] = range(1, 100 + 1)
-
-// 입력 데이터 가공
-let result = fold(numbers, (result, value) => result + value, 0)
-console.log(result)
+console.log(oddSum)
