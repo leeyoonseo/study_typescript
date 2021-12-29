e1c3e968a109bfc4d015f55aa8747853e7c30b27
leeyoonseo <okayoon.lee@gmail.com>
2021-12-29 20:05:29 +0900

    인덱스 연산자

--- ch05/src/index.ts
+++ ch05/src/index.ts
@@ -1,18 +1,6 @@
-let numArr: number[] = [1,2,3]
-let strArr: string[] = ['a','b','c']
-
-type IPerson = { name: string, age?: number }
-let personArr: IPerson[] = [
-  { name: 'Jack', age: 33 },
-  { name: 'Jane', age: 23 }
-]
-
-// 문자열과 배열 간 변환
-// split(구분자: string): string[]
-const split = (str: string, delim: string = ''): string[] => str.split(delim)
-console.log(split('hello'))
-console.log(split('h_e_l_l_o', '_'))
-
-const join = (strArr: string[], delim: string = ''): string => strArr.join(delim)
-console.log(['h', 'e', 'l', 'l', 'o'])
-console.log(['h', 'e', 'l', 'l', 'o'], '_')
+// 인덱스 연산자
+const numbers: number[] = [1,2,3,4,5]
+for(let index = 0; index < numbers.length; index++) {
+  const item: number = numbers[index]
+  console.log(item)
+}
